78e54c7d0c180e19604f059f8c29c4c60fe112a9
Shazia Khan <shaziya5416@gmail.com>
2022-05-18 18:15:13 +0530

    No-UI-multi-form

---
+++ block-BRaabv/code/my-app/src/index.js
@@ -0,0 +1,13 @@
+
+  
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ParentForm from './Components/ParentForm';
+import './stylesheets/style.css';
+
+ReactDOM.render(
+  <>
+    <ParentForm />
+  </>,
+  document.getElementById('root')
+);
